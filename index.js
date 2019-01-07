84057edde6656ca0bb53cbdca45661da9b74dbaf
wisdomtooth <cbtsao47@gmail.com>
2019-01-07 16:15:33 -0600

    finish MVP

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 // import your node modules
 
-const db = require('./data/db.js');
-
+const express = require("express");
+const db = require("./data/db.js");
+const server = express();
 // add your server code starting here
+server.listen(5000, () => console.log("server is running"));
+
+server.get("/api/posts", (req, res) => {
+  db.find()
+    .then(post => {
+      console.log(res);
+      res.send({ post });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+    });
+});
+
+server.get("/api/posts/:id", (req, res) => {
+  const id = req.params.id;
+  db.findById(id)
+    .then(post => {
+      if (post.length !== 0) {
+        res.send({ post });
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved." });
+    });
+});
